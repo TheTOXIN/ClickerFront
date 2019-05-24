8f8fd2c92fa1c26a10b23053f7f0989a3596058b
toxin <yuri.belousov.1998@gmail.com>
2019-05-24 17:25:02 +0300

    + tmp anim particles

--- src/app/pages/clicker/clicker.page.ts
+++ src/app/pages/clicker/clicker.page.ts
@@ -7,59 +7,82 @@ import {State} from '../../models/State';
 import {animate, animation, keyframes, state, style, transition, trigger, useAnimation} from '@angular/animations';
 
 @Component({
-  selector: 'app-clicker',
-  templateUrl: './clicker.page.html',
-  styleUrls: ['./clicker.page.scss'],
-  animations: [
-      trigger('clickState', [
-          transition('* => *', [
-              style({ transform: 'scale(1)' }),
-              animate(200, keyframes([
-                  style({ transform: 'scale(1)', offset: 0 }),
-                  style({ transform: 'scale(1.5)', offset: 0.5 }),
-                  style({ transform: 'scale(1)', offset: 1 })
-              ]))
-          ])
-      ]),
-  ]
+    selector: 'app-clicker',
+    templateUrl: './clicker.page.html',
+    styleUrls: ['./clicker.page.scss'],
+    animations: [
+        trigger('clickState', [
+            transition('* => *', [
+                style({transform: 'scale(1)'}),
+                animate(200, keyframes([
+                    style({transform: 'scale(1)', offset: 0}),
+                    style({transform: 'scale(1.5)', offset: 0.5}),
+                    style({transform: 'scale(1)', offset: 1})
+                ]))
+            ])
+        ]),
+        trigger('particleMyState', [
+            transition(':enter', [
+                style({opacity: 1}),
+                animate(1000, style({transform: 'translateY(-777%)', opacity: 0})),
+            ]),
+        ]),
+        trigger('particleMeState', [
+            transition(':enter', [
+                style({opacity: 1}),
+                animate(1000, style({transform: 'translateY(777%)', opacity: 0})),
+            ]),
+        ])
+    ]
 })
 export class ClickerPage implements OnInit {
 
-  public user: User;
-  public state: State;
+    public user: User;
+    public state: State;
 
-  maxClick = 1000;
-  isLoad = false;
+    isLoad = false;
 
-  constructor(
-    public socket: SocketService,
-    private http: HttpService,
-  ) {
-    this.user = JSON.parse(localStorage.getItem(CONST.KEY));
-  }
+    maxClick = 1000;
+    batchClick = 100;
 
-  ngOnInit() {
-    this.http.state(this.user).subscribe(state => {
-      if (state == null) { this.back(); }
-      this.state = state;
-      this.eventer();
-      this.isLoad = true;
-    });
-  }
+    myParticles = [];
+    meParticles = [];
 
-  eventer() {
-    this.socket.clickerObservable.subscribe(() => {
-        this.state.meCount++;
-    });
-  }
+    constructor(
+        public socket: SocketService,
+        private http: HttpService,
+    ) {
+        this.user = JSON.parse(localStorage.getItem(CONST.KEY));
+    }
 
-  click(event) {
-    this.state.myCount++;
-    this.socket.click(this.user.token);
-  }
+    ngOnInit() {
+        this.http.state(this.user).subscribe(state => {
+            if (state == null) { this.back(); }
+            this.state = state;
+            this.eventer();
+            this.isLoad = true;
+        });
+    }
 
-  back() {
-    localStorage.clear();
-    window.location.reload();
-  }
+    eventer() {
+        this.socket.clickerObservable.subscribe(() => {
+            this.state.meCount++;
+
+            if (this.meParticles.length === this.batchClick) { this.meParticles = []; }
+            this.meParticles.push(this.state.meCount);
+        });
+    }
+
+    click(event) {
+        this.state.myCount++;
+        this.socket.click(this.user.token);
+
+        if (this.myParticles.length === this.batchClick) { this.myParticles = []; }
+        this.myParticles.push(this.state.myCount);
+    }
+
+    back() {
+        localStorage.clear();
+        window.location.reload();
+    }
 }
